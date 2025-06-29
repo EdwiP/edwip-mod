4c5788d520c3888a50ce040bb5ea970d2d868580
EdwiP <nhutp6003@gmail.com>
2025-06-29 12:56:53 +0900

    Updated lang.

--- src/main/java/com/edwip/Menu/ConfigScreen.java
+++ src/main/java/com/edwip/Menu/ConfigScreen.java
@@ -64,7 +64,8 @@ public class ConfigScreen {
                 .startEnumSelector(Text.translatable("edwipmod.config.kick.letter"), Prefixes.lettersCapitalization.class, ModConfig.kickReasonLetters)
                 .setDefaultValue(Prefixes.lettersCapitalization.LOWER_ALL)
                 .setSaveConsumer(newValue -> ModConfig.kickReasonLetters = newValue)
-                .setTooltip(Text.translatable("edwipmod.config.kick.letter.tool_tip").append("\n\n").append(Text.translatable("edwipmod.prefix.letter")))
+                .setTooltip(Text.translatable("edwipmod.config.kick.letter.tool_tip")
+                        .append("\n\n").append(Text.translatable("edwipmod.prefix.letter")))
                 .build()
         );
         kick.addEntry(entryBuilder
@@ -78,147 +79,132 @@ public class ConfigScreen {
                 .startEnumSelector(Text.translatable("edwipmod.config.kick.letter.second_command.letter"), Prefixes.lettersCapitalization.class, ModConfig.kickSecondLetters)
                 .setDefaultValue(Prefixes.lettersCapitalization.LOWER_ALL)
                 .setSaveConsumer(newValue -> ModConfig.kickSecondLetters = newValue)
-                .setTooltip(Text.translatable("edwipmod.config.kick.letter.second_command.letter.tool_tip").append("\n\n").append(Text.translatable("edwipmod.prefix.letter")))
+                .setTooltip(Text.translatable("edwipmod.config.kick.letter.second_command.letter.tool_tip")
+                        .append("\n\n").append(Text.translatable("edwipmod.prefix.letter")))
                 .build()
         );
 
         kick.addEntry(entryBuilder
-                .startIntSlider(Text.of("Second Command Time"), ModConfig.kickSecondTime, 0, 1000)
-                .setDefaultValue(500)
+                .startIntSlider(Text.translatable("edwipmod.config.kick.letter.second_command.time"), ModConfig.kickSecondTime, 0, 1000/20)
+                .setDefaultValue(500/20)
                 .setSaveConsumer(newValue -> ModConfig.kickSecondTime = newValue)
-                .setTooltip(Text.of("""
-                        Time to delay the second command execution (in ms) after kicking."""))
+                .setTooltip(Text.translatable("edwipmod.config.kick.letter.second_command.time.tool_tip"))
                 .build()
         );
         // Warn
-        ConfigCategory warn = builder.getOrCreateCategory(Text.of("Warn Settings"));
+        ConfigCategory warn = builder.getOrCreateCategory(Text.translatable("edwipmod.config.warn.title"));
         warn.addEntry(entryBuilder
-                .startTextDescription(Text.of("General:"))
+                .startTextDescription(Text.translatable("edwipmod.title.general"))
                 .build()
         );
         warn.addEntry(entryBuilder
-                .startTextField(Text.of("Warn Reason"), ModConfig.warnPrefix)
+                .startTextField(Text.translatable("edwipmod.config.warn.reason"), ModConfig.warnPrefix)
                 .setDefaultValue("Please stop <REASON>, if you continue you will be banned")
                 .setSaveConsumer(newValue -> ModConfig.warnPrefix = newValue)
-                .setTooltip(Text.of("""
-                        Warn reason (replaced by <REASON>).
-                        /mpwarn <Player> <Reason>
-                        <Reason> is optional."""))
+                .setTooltip(Text.translatable("edwipmod.config.warn.reason.tool_tip"))
                 .build()
         );
         warn.addEntry(entryBuilder
-                .startEnumSelector(Text.of("Warn Letters Capitalization"), Prefixes.lettersCapitalization.class, ModConfig.warnReasonLetters)
+                .startEnumSelector(Text.translatable("edwipmod.config.warn.letter"), Prefixes.lettersCapitalization.class, ModConfig.warnReasonLetters)
                 .setDefaultValue(Prefixes.lettersCapitalization.LOWER_ALL)
                 .setSaveConsumer(newValue -> ModConfig.warnReasonLetters = newValue)
-                .setTooltip(Text.of("""
-                        Change letters capitalization.
-                        
-                        Example: heLLo WOrlD!
-                        Lower All -> hello world!
-                        Upper All -> HELLO WORLD!
-                        First Letter -> Hello world!
-                        First Every Letter -> Hello World!
-                        
-                        Option not in the list will keep Reason as the same."""))
+                .setTooltip(Text.translatable("edwipmod.config.warn.letter.tool_tip")
+                        .append("\n\n").append(Text.translatable("edwipmod.prefix.letter")))
                 .build()
         );
 
         // Discord Chat Log
-        ConfigCategory discord = builder.getOrCreateCategory(Text.of("Discord Chat Log"));
+        ConfigCategory discord = builder.getOrCreateCategory(Text.translatable("edwipmod.config.discord.category"));
         discord.addEntry(entryBuilder
-                .startTextDescription(Text.of("General:"))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Main Discord Webhook URL"), ModConfig.discordMainWebhookUrl)
-                .setDefaultValue("")
-                .setSaveConsumer(newValue -> ModConfig.discordMainWebhookUrl = newValue)
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Time Stamp"), ModConfig.discordTimeStamp)
-                .setDefaultValue("<t:TIMESTAMP:T>")
-                .setSaveConsumer(newValue -> ModConfig.discordTimeStamp = newValue)
-                .setTooltip(Text.of("""
-                        Time stamp format at the beginning of each message. Leave empty for no time stamp.
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("User Name"), ModConfig.discordUserName)
-                .setDefaultValue("You")
-                .setSaveConsumer(newValue -> ModConfig.discordUserName = newValue)
-                .setTooltip(Text.of("""
-                        Replace <PLAYER>. Can be used in 4 options below.
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Open Game Message"), ModConfig.discordOpenGameMessage)
-                .setDefaultValue("Opened the game.")
-                .setSaveConsumer(newValue -> ModConfig.discordOpenGameMessage = newValue)
-                .setTooltip(Text.of("""
-                        Message after opening game. Leave empty for no message.
-                        Can be used with <PLAYER>
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Close Game Message"), ModConfig.discordCloseGameMessage)
-                .setDefaultValue("Closed the game.")
-                .setSaveConsumer(newValue -> ModConfig.discordCloseGameMessage = newValue)
-                .setTooltip(Text.of("""
-                        Message after closing game. Leave empty for no message.
-                        Can be used with <PLAYER>
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Join Server Message"), ModConfig.discordJoinServerMessage)
-                .setDefaultValue("Joined server <SERVER>.")
-                .setSaveConsumer(newValue -> ModConfig.discordJoinServerMessage = newValue)
-                .setTooltip(Text.of("""
-                        Message after joining servers. Leave empty for no message.
-                        Can be used with <PLAYER> and <SERVER>
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextField(Text.of("Leave Server Message"), ModConfig.discordLeaveServerMessage)
-                .setDefaultValue("Left server <SERVER>.")
-                .setSaveConsumer(newValue -> ModConfig.discordLeaveServerMessage = newValue)
-                .setTooltip(Text.of("""
-                        Message after leaving servers. Leave empty for no message.
-                        Can be used with <PLAYER> and <SERVER>
-                        Can be used with Discord format."""))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startTextDescription(Text.of("Mineplay:"))
-                .build()
-        );
-        discord.addEntry(entryBuilder
-                .startStrList(Text.of("Exclude Regexes"), ModConfig.discordMineplayPrivacy)
-                .setDefaultValue(Prefixes.skipPatterns)
-                .setSaveConsumer(newValue -> ModConfig.discordMineplayPrivacy = newValue)
-                .setTooltip(Text.of("""
-                        Exclude messages with matched regexes.
-                        Visit regex101.com to know more about regex."""))
+                .startTextDescription(Text.translatable("edwipmod.title.general"))
                 .build());
 
         discord.addEntry(entryBuilder
-                .startStrList(Text.of("Regex Matchers"), ModConfig.regexPatterns)
+                .startTextField(Text.translatable("edwipmod.config.discord.webhook"), ModConfig.discordMainWebhookUrl)
+                .setDefaultValue("")
+                .setSaveConsumer(newValue -> ModConfig.discordMainWebhookUrl = newValue)
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.timestamp"), ModConfig.discordTimeStamp)
+                .setDefaultValue("<t:TIMESTAMP:T>")
+                .setSaveConsumer(newValue -> ModConfig.discordTimeStamp = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.timestamp.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.username"), ModConfig.discordUserName)
+                .setDefaultValue("You")
+                .setSaveConsumer(newValue -> ModConfig.discordUserName = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.username.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.open"), ModConfig.discordOpenGameMessage)
+                .setDefaultValue("Opened the game.")
+                .setSaveConsumer(newValue -> ModConfig.discordOpenGameMessage = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.open.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.no_message"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.player"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.close"), ModConfig.discordCloseGameMessage)
+                .setDefaultValue("Closed the game.")
+                .setSaveConsumer(newValue -> ModConfig.discordCloseGameMessage = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.close.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.no_message"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.player"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.join"), ModConfig.discordJoinServerMessage)
+                .setDefaultValue("Joined server <SERVER>.")
+                .setSaveConsumer(newValue -> ModConfig.discordJoinServerMessage = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.join.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.no_message"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.player"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextField(Text.translatable("edwipmod.config.discord.leave"), ModConfig.discordLeaveServerMessage)
+                .setDefaultValue("Left server <SERVER>.")
+                .setSaveConsumer(newValue -> ModConfig.discordLeaveServerMessage = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.leave.tooltip")
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.no_message"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.player"))
+                        .append("\n").append(Text.translatable("edwipmod.prefix.discord.discord_format")))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startTextDescription(Text.translatable("edwipmod.title.mineplay"))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startStrList(Text.translatable("edwipmod.config.discord.exclude"), ModConfig.discordMineplayPrivacy)
+                .setDefaultValue(Prefixes.skipPatterns)
+                .setSaveConsumer(newValue -> ModConfig.discordMineplayPrivacy = newValue)
+                .setTooltip(Text.translatable("edwipmod.config.discord.exclude.tooltip"))
+                .build());
+
+        discord.addEntry(entryBuilder
+                .startStrList(Text.translatable("edwipmod.config.discord.matchers"), ModConfig.regexPatterns)
                 .setDefaultValue(Prefixes.inputRegex)
                 .setSaveConsumer(newValue -> ModConfig.regexPatterns = newValue)
                 .build());
 
         discord.addEntry(entryBuilder
-                .startStrList(Text.of("Regex Outputs"), ModConfig.regexResults)
+                .startStrList(Text.translatable("edwipmod.config.discord.outputs"), ModConfig.regexResults)
                 .setDefaultValue(Prefixes.outputFormat)
                 .setSaveConsumer(newValue -> ModConfig.regexResults = newValue)
-                .setTooltip(Text.of("""
-                        Can be used with Discord format. Each group is captured by (1), (2), ect."""))
+                .setTooltip(Text.translatable("edwipmod.config.discord.outputs.tooltip"))
                 .build());
+
 
         builder.setSavingRunnable(ModConfig::save);
         return builder.build();
